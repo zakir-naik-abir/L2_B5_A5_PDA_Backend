8ca801b61b9e858b6670255eb87fcd9bd2553077
Zakir Hasan <zakirhasanabir@gmail.com>
2025-07-29 16:47:30 +0600

    added appError notFound and cathAsync file

---
+++ src/app/utils/catchAsync.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+export const catchAsync = (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
+  Promise.resolve(fn(req, res, next)).catch((err: any) =>{
+    next(err)
+  })
+};
